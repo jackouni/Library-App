5024b6eeabcfb475458fb220756b4054491fe5bb
Jack Sebben <jacksebben11@hotmail.com>
2023-08-08 09:06:12 -0400

    Made a function (addBooksToShelf) that will loop through the library array with books and display them to the 'bookShelf'. Added stylings in the CSS for these displayed books to use. Now to make a form that will take user input to then create a new book element upon submission.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
+
 const addBtn = document.getElementById('add-book')
 const bookShelf = document.getElementById('book-shelf')
-const stats = document.getElementById('stats')
 
 let book1 = {
     title: "New Book",
@@ -24,6 +24,13 @@ let book3 = {
     read: false
 }
 
+let book4 = {
+    title: "Wizard of Oz",
+    author: "idk who", 
+    pages: 201,
+    read: true
+}
+
 function Book(title, author, pages, read) {
     this.title = title
     this.author = author
@@ -35,4 +42,38 @@ function addBookToLibrary() {
 
 }
 
-let library = [book1, book2, book3] ; 
+let library = [book1, book2, book3, book4] ; 
+
+
+function addBooksToShelf() {
+    library.forEach(function(book) {
+        let bookCard = document.createElement('div')
+        bookCard.className = 'book-card'
+        bookShelf.appendChild(bookCard)
+
+        bookTitle = document.createElement('h1')
+        bookTitle.className = 'book-titles'
+        bookTitle.innerText = book.title
+        bookCard.appendChild(bookTitle)
+
+        bookAuthor = document.createElement('p')
+        bookAuthor.className = 'book-authors'
+        bookAuthor.innerText = `Author: ${book.author}`
+        bookCard.appendChild(bookAuthor)
+
+        bookPages = document.createElement('p')
+        bookPages.className = 'book-pages'
+        bookPages.innerText = `Pages: ${book.Pages}`
+        bookCard.appendChild(bookPages)
+
+        bookReadContainer = document.createElement('span')
+        bookReadContainer.className = 'book-read-container'
+        bookReadContainer.innerText = "Read: "
+        bookCard.appendChild(bookReadContainer)
+
+        bookReadBtn = document.createElement('button')
+        bookReadBtn.className = 'book-read-btn'
+        bookReadBtn.innerText = book.read
+        bookReadContainer.appendChild(bookReadBtn)
+    })
+}
