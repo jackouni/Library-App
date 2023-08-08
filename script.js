438f79114156e5906a9ea98cf8d06ba1df4ba2d8
Jack Sebben <jacksebben11@hotmail.com>
2023-08-08 08:24:27 -0400

    Decided to not get ahead of myself and simplified the layout to focus more on the functionality first. Removed the stats bar (for now), and decided to add some dummy-books to the library in the JS file. The goal is to loop through the library array and post a card representing these books.

--- script.js
+++ script.js
@@ -1,4 +1,38 @@
 const addBtn = document.getElementById('add-book')
 const bookShelf = document.getElementById('book-shelf')
 const stats = document.getElementById('stats')
- 
+
+let book1 = {
+    title: "New Book",
+    author: "Myself", 
+    pages: 189,
+    read: false
+}
+
+
+let book2 = {
+    title: "Second",
+    author: "Gary", 
+    pages: 400,
+    read: true
+}
+
+let book3 = {
+    title: "New Book the Third",
+    author: "Patrick Star", 
+    pages: 98,
+    read: false
+}
+
+function Book(title, author, pages, read) {
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.read = read
+}
+
+function addBookToLibrary() {
+
+}
+
+let library = [book1, book2, book3] ; 
